7758d6f44664caa73c86b1be025bd05be9395dd5
c7blackjack <lloydtravis@gmail.com>
2020-02-01 04:56:43 -0800

    added particle.js

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -1,14 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Footer extends Component {
-    render() {
-        return (
-            <footer>
-                This is a footer!!
-            </footer>
- 
-        );
-    }
+  render() {
+    return (
+      <footer>
+        <div className="footer-top">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3 col-sm-6 col-xs-12 segment-one">
+                <h3>LloydDM</h3>
+                <p>
+                  Aliquip ad sint voluptate ullamco Lorem Lorem magna elit sint
+                  laboris. Laborum minim mollit voluptate officia incididunt
+                  adipisicing sit nostrud nostrud magna.
+                </p>
+              </div>
+              <div className="col-md-3 col-sm-6 col-xs-12 segment-two">
+                <h2>LloydDM</h2>
+                <ul>
+                  <li>
+                    <a href="#">Something 1</a>
+                  </li>
+                  <li>
+                    <a href="#">Something 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Something 3</a>
+                  </li>
+                  <li>
+                    <a href="#">Something 4</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-md-3 col-sm-6 col-xs-12 segment-three">
+                <h2>Follow Me</h2>
+                <p>
+                  Please follow me on my Social Media Profile to keep in contact
+                </p>
+                <a href="#">
+                  <i className="fa fa-facebook"></i>
+                </a>
+                <a href="#">
+                  <i className="fa fa-twitter"></i>
+                </a>
+                <a href="#">
+                  <i className="fa fa-linkedin"></i>
+                </a>
+                <a href="#">
+                  <i className="fa fa-instagram"></i>
+                </a>
+              </div>
+              <div className="col-md-3 col-sm-6 col-xs-12 segment-four">
+                <h2>Contact Me</h2>
+                <p>LloydTravis@gmail.com 661-414-4027</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p className="footer-bottom-text">
+          All Rights Reserved by &copy;Travis Lloyd 2020
+        </p>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
-import {
-    Link
-} from 'react-router-dom';
-import Logo from '../../Logo2.png'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../Logo2.png";
 
 class Header extends Component {
-    render() {
-        return (
-            <header>
+  render() {
+    return (
+      <header>
+        <div className="logo">
+          <h1>Lloyd | Digital Media</h1>
+          {/*<img src={Logo} />*/}
+        </div>
 
-                <div className="logo">
-                <img src={Logo} />
-                </div>
-
-                <nav>
-                    <ul>
-                        <li className="first">
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/Projects">Projects</Link>
-                        </li>
-                        <li>
-                        <Link to="/About">About</Link>
-                        </li>
-                        <li className="last">
-                        <Link to="/Contact">Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-            </header>
-        );
-    }
+        <nav>
+          <ul>
+            <li className="first">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li className="last">
+              <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default Header;
