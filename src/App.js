ff62ae996c5bfb41e481806ff564e03033d646d3
c7blackjack <lloydtravis@gmail.com>
2020-02-08 03:33:27 -0800

    Fixed Mobile navigation and added animation to menu

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 //COMPONENTS
-import Header from './components/headerComponent/header';
-import Footer from './components/footerComponent/footer';
-import Homepage from './components/pages/homePage';
-import About from './components/pages/about';
-import Projects from './components/pages/projects';
-import Contact from './components/pages/contact';
+import Header from "./components/headerComponent/header";
+import Footer from "./components/footerComponent/footer";
+import Homepage from "./components/pages/homePage";
+import About from "./components/pages/about";
+import Projects from "./components/pages/projects";
+import Contact from "./components/pages/contact";
+
 //INCLUDES
-import './Assets/css/default.min.css';
+import "./Assets/css/default.min.css";
 
 class App extends Component {
   render() {
-      return (
-        <Router>
-          <div className="App">
-
+    return (
+      <Router>
+        <div className="App">
           <Header />
-            
-            <Route exact path='/' component = {Homepage} />
-            <Route exact path='/About' component = {About} />
-            <Route exact path='/Projects' component = {Projects} />
-            <Route exact path='/Contact' component = {Contact} />
+
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Projects" component={Projects} />
+          <Route exact path="/Contact" component={Contact} />
 
           <Footer />
-
-          </div>
-        </Router>
-      );
+        </div>
+      </Router>
+    );
   }
 }
 
--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -2,17 +2,68 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 // import Logo from "../../Logo2.png";
 
+// const navSlide = () => {
+//   const burger = document.querySelector(".burger");
+//   const nav = document.querySelector(".nav-links");
+
+//   burger.addEventListener("click", () => {
+//     nav.classList.toggle("nav-active");
+//   });
+// };
+// navSlide();
+
 class Header extends Component {
+  clickHandler() {
+    const navLinks = document.querySelectorAll(".nav-links li");
+    const nav = document.querySelector(".nav-links");
+    const burger = document.querySelector(".burger");
+
+    //Mobile Menu Toggler
+    nav.classList.toggle("nav-active");
+
+    //Link Animation
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        console.log("Testy1");
+        link.style.animation = "";
+      } else {
+        console.log("Testy2");
+        link.style.animation = `navLinkFadeIn 0.5s ease forwards ${index / 7 +
+          0.3}s`;
+      }
+      console.log(link.style.animation);
+    });
+
+    //  Attempt at switching between all different possibilities
+
+    //   if (link.style.animation === "navLinkFadeIn") {
+    //     console.log("Testy1");
+    //     link.style.animation = `navLinkFadeIn 0.5s ease forwards ${index / 7 +
+    //       0.3}s`;
+    //   } else if (link.style.animation === "navLinkFadeOut") {
+    //     console.log("Testy2");
+    //     link.style.animation = `navLinkFadeIn 0.5s ease forwards ${index / 7 +
+    //       0.3}s`;
+    //   } else if (link.style.animation === "") {
+    //     console.log("Testy3");
+    //     link.style.animation = `navLinkFadeIn 0.5s ease forwards ${index / 7 +
+    //       0.3}s`;
+    //   }
+    //   console.log(link.style.animation);
+    // });
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  }
+
   render() {
     return (
       <header>
         <div className="logo">
-          <h1>Lloyd | Digital Media</h1>
+          <h1>Travis Lloyd</h1>
           {/*<img src={Logo} />*/}
         </div>
-
-        <nav>
-          <ul>
+        <nav className="nav">
+          <ul className="nav-links">
             <li className="first">
               <Link to="/">Home</Link>
             </li>
@@ -26,6 +77,11 @@ class Header extends Component {
               <Link to="/Contact">Contact</Link>
             </li>
           </ul>
+          <div className="burger" onClick={this.clickHandler}>
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
         </nav>
       </header>
     );
