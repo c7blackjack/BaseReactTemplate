36d3eb7e1d0c5abc7c53df939de4072e48dcf85f
c7blackjack <lloydtravis@gmail.com>
2020-03-27 08:45:43 -0700

    heroku attempt

--- server.js
+++ server.js
@@ -11,23 +11,31 @@ const PORT = normalizePort(process.env.PORT || 5000);
 
 const app = express();
 
-const dev = app.get("env") !== "production";
-
-if (!dev) {
-  app.disable("x-powered-by");
-  app.use(compression());
-  app.use(morgan("common"));
-
-  app.use(express.static(path.resolve(__dirname, "build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-if (dev) {
-  app.use(morgan("dev"));
-}
+//const dev = app.get("env") !== "production";
+
+// if (!dev) {
+//   app.disable("x-powered-by");
+//   app.use(compression());
+//   app.use(morgan("common"));
+
+//   app.use(express.static(path.resolve(__dirname, "build")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+// if (dev) {
+//   app.use(morgan("dev"));
+// }
 
 const server = createServer(app);
 
